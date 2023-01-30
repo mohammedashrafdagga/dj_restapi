70f4b1d69bd39446dd38602ff4064c4aebb15c51
Mohammed Dagga <47271587+mohammedashrafdagga@users.noreply.github.com>
2023-01-30 19:52:01 +0200

    create first api view

--- py_client/basic.py
+++ py_client/basic.py
@@ -3,14 +3,17 @@ import requests
 
 # create endpoint
 # endpoint = 'https://httpbin.org/anything'
-endpoint = 'http://localhost:8000/'  # http://127.0.0.1:8000/
+endpoint = 'http://localhost:8000/api/'  # http://127.0.0.1:8000/
 
 
 # send request
 get_response = requests.get(endpoint)
 
-# get text from application  response
-# print(get_response.text)
+#  get Json from application  response
+print(get_response.json())
 
 # status code
 print(get_response.status_code)
+
+# print a message form json
+print(get_response.json()['message'])
