e96dda3b31036603091044a0c552fd51b954bc60
Mohammed Dagga <47271587+mohammedashrafdagga@users.noreply.github.com>
2023-01-31 10:22:57 +0200

    django model instance to dict by build in method

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,14 +1,18 @@
 from django.http import JsonResponse
 from product.models import Product
+from django.forms.models import model_to_dict
 
 
 def api_home(request, *args, **kwargs):
     model_data = Product.objects.all().order_by('?').first()
     data = {}
     if model_data:
-        data['id'] = model_data.id
-        data['title'] = model_data.title
-        data['content'] = model_data.content
-        data['price'] = model_data.price
+        # data['id'] = model_data.id
+        # data['title'] = model_data.title
+        # data['content'] = model_data.content
+        # data['price'] = model_data.price
+
+        # clean up way
+        data = model_to_dict(model_data, fields=['id', 'title', 'price'])
 
     return JsonResponse(data)
