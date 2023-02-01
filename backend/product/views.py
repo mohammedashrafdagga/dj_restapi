2ee847014525a9313889cb12738e687250a09b37
Mohammed Dagga <47271587+mohammedashrafdagga@users.noreply.github.com>
2023-02-01 21:37:11 +0200

    cleanup view file

--- backend/product/views.py
+++ backend/product/views.py
@@ -1,9 +1,9 @@
 from rest_framework import generics, mixins
 from .models import Product
 from .serializers import ProductSerializer
-# from rest_framework.response import Response
-# from rest_framework.decorators import api_view
-# from django.shortcuts import get_object_or_404
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from django.shortcuts import get_object_or_404
 # create product api view
 
 
@@ -21,19 +21,10 @@ class ProductListCreateAPIView(generics.ListCreateAPIView):
         serializer.save(content=content)
 
 
-# list and create product view
-product_list_create_view = ProductListCreateAPIView.as_view()
-
 # detail View
-
-
 class ProductDetailApiView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    # lookup_filed = id default
-
-
-product_detail_view = ProductDetailApiView.as_view()
 
 
 # Update View
@@ -48,9 +39,6 @@ class ProductUpdateApiView(generics.UpdateAPIView):
             instance.content = instance.title
 
 
-product_update_view = ProductUpdateApiView.as_view()
-
-
 # destroy View
 class ProductDeleteApiView(generics.DestroyAPIView):
     queryset = Product.objects.all()
@@ -62,10 +50,9 @@ class ProductDeleteApiView(generics.DestroyAPIView):
         return super().perform_destroy(instance)
 
 
-product_delete_view = ProductDeleteApiView.as_view()
-
-
 # Using Mixins View
+# (This Class merge List, Retrieve and Create Class API View) in One Class
+
 class ProductMixinsView(
     mixins.CreateModelMixin,
     mixins.RetrieveModelMixin,
@@ -94,30 +81,46 @@ class ProductMixinsView(
         serializer.save(content=content)
 
 
-product_mixin_view = ProductMixinsView.as_view()
-
 # Function Based View (List, Retrieve, Create)
-# @api_view(['GET', 'POST'])
-# def product_alt_view(request, pk=None, *args, **kwargs):
-#     method = request.method
-#     # request -> get (detail or list)
-#     if method == 'GET':
-#         if pk:  # not None -> detail View
-#             qs = get_object_or_404(Product, pk=pk)
-#             data = ProductSerializer(qs).data
-#             return Response(data)
+@api_view(['GET', 'POST'])
+def product_alt_view(request, pk=None, *args, **kwargs):
+    method = request.method
+    # request -> get (detail or list)
+    if method == 'GET':
+        if pk:  # not None -> detail View
+            qs = get_object_or_404(Product, pk=pk)
+            data = ProductSerializer(qs).data
+            return Response(data)
 
-#         # list View
-#         qs = Product.objects.all()
-#         data = ProductSerializer(qs, many=True).data
-#         return Response(data)
+        # list View
+        qs = Product.objects.all()
+        data = ProductSerializer(qs, many=True).data
+        return Response(data)
 
-#     elif method == 'POST':  # create new instance from Product
-#         serializer = ProductSerializer(data=request.data)
-#         if serializer.is_valid(raise_exception=True):
-#             title = serializer.validated_data.get('title')
-#             content = serializer.validated_data.get('content') or None
-#             if content is None:
-#                 content = title
-#             serializer.save(content=content)
-#         return Response(serializer.data)
+    elif method == 'POST':  # create new instance from Product
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            title = serializer.validated_data.get('title')
+            content = serializer.validated_data.get('content') or None
+            if content is None:
+                content = title
+            serializer.save(content=content)
+        return Response(serializer.data)
+
+
+# for more cleanup, Move all Variable Here
+# list and create product view
+product_list_create_view = ProductListCreateAPIView.as_view()
+
+# Detail view
+product_detail_view = ProductDetailApiView.as_view()
+
+# Update View
+product_update_view = ProductUpdateApiView.as_view()
+
+# Delete View
+product_delete_view = ProductDeleteApiView.as_view()
+
+
+# Product Mixins View
+product_mixin_view = ProductMixinsView.as_view()
