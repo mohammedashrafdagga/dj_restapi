16bdadd0c6e0b033bcbf506b853d1b4cf8d8712e
Mohammed Dagga <47271587+mohammedashrafdagga@users.noreply.github.com>
2023-02-02 11:31:58 +0200

    session authentication and permissions

--- backend/product/views.py
+++ backend/product/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics, mixins
+from rest_framework import generics, mixins, authentication, permissions
 from .models import Product
 from .serializers import ProductSerializer
 from rest_framework.response import Response
@@ -10,6 +10,8 @@ from django.shortcuts import get_object_or_404
 class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    authentication_classes = [authentication.SessionAuthentication]
+    permission_classes = [permissions.DjangoModelPermissions]
 
     def perform_create(self, serializer):
         # serializer.save(user = self.request.user)
