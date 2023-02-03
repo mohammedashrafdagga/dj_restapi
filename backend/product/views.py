4b712e20d79a382da062c7ce8b6a552d3fb2cf8b
Mohammed Dagga <47271587+mohammedashrafdagga@users.noreply.github.com>
2023-02-03 18:04:14 +0200

    default django rest_framework settings

--- backend/product/views.py
+++ backend/product/views.py
@@ -12,10 +12,7 @@ from api.authentication import TokenAuthentication
 class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    authentication_classes = [
-        authentication.SessionAuthentication,
-        TokenAuthentication
-    ]
+
     permission_classes = [permissions.IsAdminUser, IsStaffProductEditor]
 
     def perform_create(self, serializer):
