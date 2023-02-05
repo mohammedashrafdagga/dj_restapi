bbe6c9f4f146eb360b12154dfd10799b2cccc2fc
Mohammed Dagga <47271587+mohammedashrafdagga@users.noreply.github.com>
2023-02-05 20:11:11 +0200

    Build Django Search Based for our Product API

--- backend/restapi/urls.py
+++ backend/restapi/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('api/', include('api.urls')),  # localhost:8000/api
     path('api/product/', include('product.urls')), # localhost:8000/api/product/
-    path('api/v2/', include('restapi.router'))
+    path('api/search/', include('search.urls')),
+    path('api/v2/', include('restapi.router')),
 ]
